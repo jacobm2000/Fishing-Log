1f3327b4a8303de5be070e0993f7b8d7584313a9
jacobm2000 <maurerja2000@gmail.com>
2023-07-26 11:26:13 -0400

    added more functionality to new user page
    made it so new users must pick a unique name and the user must be logged in to see the home page

--- flask-main.py
+++ flask-main.py
@@ -1,10 +1,13 @@
 
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect,flash,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///fish.sqlite3'
+app.config['SECRET_KEY']='632h3232ss'
 db = SQLAlchemy(app)
+app.user=""
+
 class fishLog(db.Model):
    id = db.Column('fish_id', db.Integer, primary_key = True)
    name= db.Column('name' ,db.String(50))
@@ -19,29 +22,44 @@ def __init__(self, name, city, addr,pin):
    self.name = name
 db.create_all()
 
-@app.route("/",)
+@app.route("/login")
 def login() :
         return render_template("login.html")
     
 @app.route("/newacc",methods=["POST","GET"])
 def newAcc() :
     if request.method=="POST":
-        user=str(request.form["username"])
-        passw=str(request.form["password"])
-        
    
-        new_user=accounts(
-            username=user,
-            password=passw
-            )
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect('/home')
+        app.user=str(request.form["username"])
+        passw=str(request.form["password"])
+        checkUser=accounts.query.filter_by(username=app.user)
+        
+        #if user is not in db then it will throw an exception and the user can be added
+        try:
+           checkUser[0]
+           flash('username taken')
+           return redirect('/newacc')
+           
+          
+           
+        except:
+            new_user=accounts(
+                username=app.user,
+                password=passw
+                )
+            db.session.add(new_user)
+            db.session.commit()
+      
+            return render_template("home.html",username=app.user)
+           
+        
     else:       
         return  render_template("newAcc.html")
         
 @app.route("/home",methods=["POST","GET"])
 def home() :
+    if (app.user==""):
+        return redirect("/login")
     if request.method=="POST":
         fishName= str(request.form["fish"])
         new_fish=fishLog(
@@ -49,10 +67,11 @@ def home() :
             )
         db.session.add(new_fish)
         db.session.commit()
-        return redirect('/home')
+        return redirect(url_for('home',username=app.user))
     else:
         fishList= fishLog.query
-        return render_template("home.html",fishList=fishList)
+        print(2)
+        return render_template("home.html",fishList=fishList,username=app.user)
     
 if __name__== "__main__":
     app.run(debug=False)
