b3f2f0da3c2924eaf3664226b4a5e5e7fcb7adeb
jacobm2000 <maurerja2000@gmail.com>
2023-07-26 19:43:59 -0400

    Allowed for users to login
    Allow users to login, checking if they  are in the db. and username is now stored in a session. functionality to logout and clear the session has also been implemented.

--- flask-main.py
+++ flask-main.py
@@ -1,12 +1,15 @@
 
-from flask import Flask, render_template,request,redirect,flash,url_for
+from flask import Flask, render_template,request,redirect,flash,url_for,session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///fish.sqlite3'
 app.config['SECRET_KEY']='632h3232ss'
 db = SQLAlchemy(app)
-app.user=""
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 class fishLog(db.Model):
    id = db.Column('fish_id', db.Integer, primary_key = True)
@@ -22,17 +25,32 @@ def __init__(self, name, city, addr,pin):
    self.name = name
 db.create_all()
 
-@app.route("/login")
-def login() :
-        return render_template("login.html")
+@app.route("/login",methods=["GET","POST"])
+def login():
+         
+    if(request.method=="POST"):
+        print("moo")
+        session['user']=str(request.form["username"])
+        passw=str(request.form["password"])
+        checkUser=accounts.query.filter_by(username=session['user'],password=passw)
+        
+            #if user is not in db then it will throw an exception and the user can be added
+        try:
+               checkUser[0]
+               return redirect(url_for(".home",username=session['user']))
+        except:
+            flash("password or username incorrect")
+            return redirect('/login')
+    else:
+       return render_template("login.html")
     
 @app.route("/newacc",methods=["POST","GET"])
 def newAcc() :
     if request.method=="POST":
    
-        app.user=str(request.form["username"])
+        session['user']=str(request.form["username"])
         passw=str(request.form["password"])
-        checkUser=accounts.query.filter_by(username=app.user)
+        checkUser=accounts.query.filter_by(username=session['user'])
         
         #if user is not in db then it will throw an exception and the user can be added
         try:
@@ -44,13 +62,13 @@ def newAcc() :
            
         except:
             new_user=accounts(
-                username=app.user,
+                username=session['user'],
                 password=passw
                 )
             db.session.add(new_user)
             db.session.commit()
       
-            return render_template("home.html",username=app.user)
+            return redirect(url_for(".home",username=session['user']))
            
         
     else:       
@@ -58,20 +76,27 @@ def newAcc() :
         
 @app.route("/home",methods=["POST","GET"])
 def home() :
-    if (app.user==""):
+    # an error means the user is not signed in so they are directed to the login page
+    try:
+        if request.method=="POST":
+            if (request.form['submit_button']=='submit'):
+                fishName= str(request.form["fish"])
+                new_fish=fishLog(
+                    name=fishName
+                    )
+                db.session.add(new_fish)
+                db.session.commit()
+                return redirect(url_for('home',username=session['user']))
+            
+            #if the user logs out their session is cleared and they are returned to login screen
+            if (request.form['submit_button']=='logout'):
+                session.clear()
+                return redirect("/login")
+        else:
+            fishList= fishLog.query
+            return render_template("home.html",fishList=fishList,username=session['user'])
+    except:
         return redirect("/login")
-    if request.method=="POST":
-        fishName= str(request.form["fish"])
-        new_fish=fishLog(
-            name=fishName
-            )
-        db.session.add(new_fish)
-        db.session.commit()
-        return redirect(url_for('home',username=app.user))
-    else:
-        fishList= fishLog.query
-        print(2)
-        return render_template("home.html",fishList=fishList,username=app.user)
     
 if __name__== "__main__":
     app.run(debug=False)
